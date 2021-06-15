fb897c5924db93e47ba85a83e6c592df68e1b4c9
venkat-rajgopal <venkatramani.r@gmail.com>
2021-06-15 21:58:50 +0200

    alert email

--- alerts.py
+++ alerts.py
@@ -1,3 +1,5 @@
+#!/home/vra/Projects/canyon/env/bin/python3 python3
+
 import requests, json
 import sys
 import os
@@ -10,7 +12,6 @@ from pprint import pprint
 f = open('creds.json')
 creds = json.load(f)
 
-url = 'https://www.canyon.com/en-de/gravel-bikes/adventure/grizl/grizl-cf-sl-6/2711.html?dwvar_2711_pv_rahmenfarbe=GY%2FBK'
 
 # all urls to scrape
 urls = { 'g7_sr' : 'https://www.canyon.com/en-de/gravel-bikes/all-road/grail/grail-7/2370.html?dwvar_2370_pv_rahmenfarbe=SR%2FBK',
@@ -23,54 +24,100 @@ urls = { 'g7_sr' : 'https://www.canyon.com/en-de/gravel-bikes/all-road/grail/gra
 }
 
 
-r = requests.get(url)
-s = re.search(r'window\.deptsfra=(.*);', r.text).group(1)
-data = json.loads(s)
+# r = requests.get(url)
+# s = re.search(r'window\.deptsfra=(.*);', r.text).group(1)
+# data = json.loads(s)
 
-results = {i['value'] : i['availability']['shippingInfo'] for i in data['productDetail']['variationAttributes'][1]['values']}
-pprint(results)
+# results = {i['value'] : i['availability']['shippingInfo'] for i in data['productDetail']['variationAttributes'][1]['values']}
+# pprint(results)
 
 def get_results(url):
     r = requests.get(url)
     s = re.search(r'window\.deptsfra=(.*);', r.text).group(1)
     data = json.loads(s)
     results = {i['value'] : i['availability']['shippingInfo'] for i in data['productDetail']['variationAttributes'][1]['values']}
-    pprint(results)
+    #pprint(results)
 
     return results
 
+def send_mail(credentials, message):
+    
+    # set mail parameters
+    text_subtype = 'plain'
+    SMTPserver = 'smtp.gmail.com'
+    destination = ['venkatramani.r@gmail.com']
+    sender = credentials['my_email']
 
-for key, value in urls.items():
-    result = get_results(url=value)
-    print(result)
+    subject_1 = "Daily Stock Summary"
+    subject_2 = "Your Bike is now Available!"
+
+    content = str(message) # message = str(message_content)
+
+    print("Sending mail")
+    try:
+
+        msg = MIMEText(content, text_subtype)
+        msg['Subject'] = subject_1
+        msg['From']    = sender # some SMTP servers will do this automatically, not all
+
+        conn = SMTP(SMTPserver)
+        conn.set_debuglevel(False)
+        conn.login(credentials['my_email'], credentials['my_pass'])
+        try:
+            conn.sendmail(sender, destination, msg.as_string())
+        finally:
+            conn.quit()
+
+        print("Mail Sent")
+
+    except:
+        sys.exit( "mail failed; %s" % "Didnt Work!" )
+
     
 
 
-text_subtype = 'plain'
-SMTPserver = 'smtp.gmail.com'
-destination = ['venkatramani.r@gmail.com']
-sender = creds['my_email']
+# Loop through the urls and collect  result for the desired size 
+# send mail
 
-content="""\
-Test message
-"""
+message_content = {}
 
-subject_1 = "Daily Stock Summary"
-subject_2 = "Your Bike is now Available!"
+for btype, value in urls.items():
+    result = get_results(url=value)
+    
+    for i, j in result.items():
+        if i == '2XS':
+            content = {'Bike' : btype, 'Size' : i, 'Status' : j, 'Link' : value}
+            message_content[btype] = content
 
 
-try:
-    msg = MIMEText(content, text_subtype)
-    msg['Subject'] = subject
-    msg['From']    = sender # some SMTP servers will do this automatically, not all
+send_mail(credentials=creds, message=message_content)
 
-    conn = SMTP(SMTPserver)
-    conn.set_debuglevel(False)
-    conn.login(creds['my_email'], creds['my_pass'])
-    try:
-        conn.sendmail(sender, destination, msg.as_string())
-    finally:
-        conn.quit()
 
-except:
-    sys.exit( "mail failed; %s" % "Didnt Work!" ) 
+# text_subtype = 'plain'
+# SMTPserver = 'smtp.gmail.com'
+# destination = ['venkatramani.r@gmail.com']
+# sender = creds['my_email']
+
+# content="""\
+# Test message
+# """
+
+# subject_1 = "Daily Stock Summary"
+# subject_2 = "Your Bike is now Available!"
+
+
+# try:
+#     msg = MIMEText(content, text_subtype)
+#     msg['Subject'] = subject_1
+#     msg['From']    = sender # some SMTP servers will do this automatically, not all
+
+#     conn = SMTP(SMTPserver)
+#     conn.set_debuglevel(False)
+#     conn.login(creds['my_email'], creds['my_pass'])
+#     try:
+#         conn.sendmail(sender, destination, msg.as_string())
+#     finally:
+#         conn.quit()
+
+# except:
+#     sys.exit( "mail failed; %s" % "Didnt Work!" ) 
